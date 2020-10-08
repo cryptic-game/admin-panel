1cc712b1ffef068e7a35f5abc536e8ebde2a703a
JannikEmmerich <mail@jannik-emmerich.de>
2020-10-08 17:46:48 +0200

    Fixed slideout animation

--- src/app/_core/animtaions/slide-from-right.ts
+++ src/app/_core/animtaions/slide-from-right.ts
@@ -1,7 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 export const slideFromRight = trigger('slideFromRight', [
-  state('void', style({ width: '0' })),
-  state('closed', style({ width: '100%' })),
-  transition(':enter, :leave', animate('200ms cubic-bezier(0.35, 0, 0.25, 1)')),
+  state('void', style({ transform: 'translateX(100%)' })),
+  state('*', style({ transform: 'translateX(0%)' })),
+  transition(':enter, :leave', animate('250ms cubic-bezier(0.35, 0, 0.25, 1)')),
 ]);
