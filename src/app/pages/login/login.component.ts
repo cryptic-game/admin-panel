2193ba9244adcd451db2ec7075d63692872de6cb
JannikEmmerich <mail@jannik-emmerich.de>
2021-01-29 21:22:03 +0100

    Fixed linting errors

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,7 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {filter, mergeMap, tap} from 'rxjs/operators';
 import {ApiService} from '../../_api/api.service';
 import {AccountService} from '../../_core/account/account.service';
-import {throwError} from "rxjs";
+import {throwError} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -30,54 +30,54 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.queryParams
-    .pipe(
-      tap((params) => (this.return = params.return)),
-      filter((params) => params.code),
-      mergeMap((params) => {
-        if (params.code) {
-          this.loading = true;
-          return this.accountService.authenticate(params.code);
+      .pipe(
+        tap((params) => (this.return = params.return)),
+        filter((params) => params.code),
+        mergeMap((params) => {
+          if (params.code) {
+            this.loading = true;
+            return this.accountService.authenticate(params.code);
+          }
+          return throwError('Hier könnte Ihre Fehlermeldung stehen!');
+        })
+      )
+      .subscribe(
+        () => {
+          // ${this.accountService.user.name}
+          console.log(
+            `Welcome, I see you have found the developer console.` +
+            '\nYou have successfully logged in with GitHub.'
+          );
+          if (this.return) {
+            this.router.navigate(this.return.split('/')).then();
+          } else {
+            this.router.navigate(['dashboard']).then();
+          }
+          this.loading = false;
+        },
+        (error) => {
+          console.warn(error);
+          this.error = error.status;
+          this.loading = false;
         }
-        return throwError("Hier könnte Ihre Fehlermeldung stehen!");
-      })
-    )
-    .subscribe(
-      () => {
-        // ${this.accountService.user.name}
-        console.log(
-          `Welcome, I see you have found the developer console.` +
-          '\nYou have successfully logged in with GitHub.'
-        );
-        if (this.return) {
-          this.router.navigate(this.return.split('/')).then();
-        } else {
-          this.router.navigate(['dashboard']).then();
-        }
-        this.loading = false;
-      },
-      (error) => {
-        console.warn(error);
-        this.error = error.status;
-        this.loading = false;
-      }
-    );
+      );
   }
 
   loginWithGithub(): void {
     this.apiService
-    .endpoint<{ client_id: string }>('authentication/oauth/client_id')
-    .then((data) => {
-      if (!data.body) {
-        return;
-      }
+      .endpoint<{ client_id: string }>('authentication/oauth/client_id')
+      .then((data) => {
+        if (!data.body) {
+          return;
+        }
 
-      let href = 'https://github.com/login/oauth/authorize'
-        + `?client_id=${data.body.client_id}`
-        + `&redirect_uri=${window.location.protocol}//${window.location.host}/login`;
-      if (this.return) {
-        href += `?return=${this.return}`;
-      }
-      window.location.href = href;
-    });
+        let href = 'https://github.com/login/oauth/authorize'
+          + `?client_id=${data.body.client_id}`
+          + `&redirect_uri=${window.location.protocol}//${window.location.host}/login`;
+        if (this.return) {
+          href += `?return=${this.return}`;
+        }
+        window.location.href = href;
+      });
   }
 }
