efaf0b6a59be9b560aacf8184b03dfc14cfa9740
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-06-17 19:01:51 +0200

    Added home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
   { path: 'website', loadChildren: () => import('./pages/website/website.module').then(m => m.WebsiteModule) },
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
   { path: '**', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) }
 ];
 
